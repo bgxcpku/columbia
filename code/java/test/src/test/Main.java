5ef82755ca51e34f8754bf2a5e5b27a1aadf0f41
Frank Wood <fwood@stat.columbia.edu>
2009-10-27 16:29:07 -0400

    added text to mcmc dp mixture document

--- code/java/test/src/test/Main.java
+++ code/java/test/src/test/Main.java
@@ -15,6 +15,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("Hello world.") ;   }
+    System.out.println("Hello world.") ;   }
 
 }
