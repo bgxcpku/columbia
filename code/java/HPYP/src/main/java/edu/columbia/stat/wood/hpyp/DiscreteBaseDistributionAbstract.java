f36bc41cfed2e0db31b883436454dc80de7e84b3
Nicholas Bartlett <bartlett@stat.columbia.edu>
2010-07-18 11:49:34 -0400

    added HPYP code base

---
+++ code/java/HPYP/src/main/java/edu/columbia/stat/wood/hpyp/DiscreteBaseDistributionAbstract.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.columbia.stat.wood.hpyp;
+
+/**
+ *
+ * @author nicholasbartlett
+ */
+public abstract class DiscreteBaseDistributionAbstract implements DiscreteBaseDistribution {
+
+    /**
+     * Dumb implementation of cdf based on the probability() method.  Can probably be
+     * more efficiently implemented by specific implementations of interface.
+     *
+     * @return cdf
+     */
+    public double[] cdf(){
+        double[] cdf;
+        int as;
+
+        as = alphabetSize();
+
+        cdf = new double[as];
+        for(int i = 0; i < as; i++){
+            cdf[i] = probability(i);
+        }
+
+        return cdf;
+    }
+
+}
