48a610a391e1b01f8028ff9caa195e597da8cd4e
Nicholas Bartlett <bartlett@stat.columbia.edu>
2010-10-30 12:20:10 -0400

    made changes to use new seating arranger

--- code/java/SequenceMemoizer/src/main/java/edu/columbia/stat/wood/sequencememoizer/v1/util/ByteRestaurant.java
+++ code/java/SequenceMemoizer/src/main/java/edu/columbia/stat/wood/sequencememoizer/v1/util/ByteRestaurant.java
@@ -139,6 +139,17 @@ public class ByteRestaurant extends ByteMap<ByteRestaurant> implements Serializa
 
             types = new_types;
             customersAndTables = new_customersAndTables;
+
+            int cu = 0;
+            int ta = 0;
+            for(int i = 0; i < customersAndTables.length; i++){
+                cu += customersAndTables[i++];
+                ta += customersAndTables[i];
+            }
+
+            assert cu == customers;
+            assert ta == tables;
+
         }
     }
 
