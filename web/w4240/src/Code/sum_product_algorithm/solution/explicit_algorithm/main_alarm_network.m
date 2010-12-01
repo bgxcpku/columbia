4cfe4f3777980a5cce60b7748bbb8586ef090513
Nicholas Bartlett <bartlett@stat.columbia.edu>
2010-12-01 17:18:42 -0500

    organizing

--- web/w4240/src/Code/sum_product_algorithm/solution/explicit_algorithm/main_alarm_network.m
+++ web/w4240/src/Code/sum_product_algorithm/solution/explicit_algorithm/main_alarm_network.m
@@ -1,80 +1,35 @@
 clear
+clear global
 
-%initialize model
-m = [.001 ; .999];
-f_b = factor(m);
+global vn_b vn_e vn_a vn_j vn_m fn_b fn_bea fn_e fn_aj fn_am;
 
-m = [.002; .998];
-f_e = factor(m);
+vn_b = node('vn_b');
+fn_b = node('fn_b');
+fn_bea = node('fn_bea');
+vn_e = node('vn_e');
+fn_e = node('fn_e');
+vn_a = node('vn_a');
+fn_aj = node('fn_aj');
+vn_j = node('vn_j');
+fn_am = node('fn_am');
+vn_m = node('vn_m');
 
-m = zeros(2,2,2);
-m(:,1,1) = [.95 .05];
-m(:,1,2) = [.94 .06];
-m(:,2,1) = [.29 .71];
-m(:,2,2) = [.001 .999];
-f_abe = factor(m);
+vn_b.setcm({fn_b, fn_bea},{ones(2,1), ones(2,1)});
+fn_b.setcm({vn_b},{ones(2,1)});
+vn_e.setcm({fn_e,fn_bea},{ones(2,1),ones(2,1)});
+fn_e.setcm({vn_e},{ones(2,1)});
+vn_a.setcm({fn_aj,fn_am,fn_bea},{ones(2,1), ones(2,1),ones(2,1)});
+fn_aj.setcm({vn_j,vn_a},{ones(2,1),ones(2,1)});
+vn_j.setcm({fn_aj},{ones(2,1)});
+fn_am.setcm({vn_m,vn_a},{ones(2,1),ones(2,1)});
+fn_bea.setcm({vn_a,vn_e,vn_b},{ones(2,1), ones(2,1),ones(2,1)});
+vn_m.setcm({fn_am},{ones(2,1)});
 
-m = zeros(2,2);
-m(:,1) = [.9 .1];
-m(:,2) = [.05 .95];
-f_ja = factor(m);
+vn_j.set_value(1);
 
-m = zeros(2,2);
-m(:,1) = [.7 .3];
-m(:,2) = [.01 .99];
-f_ma = factor(m);
+%do inference
+get_all_messages(vn_b);
+pass_all_messages(vn_b);
 
-fn_b = factor_node('fn:b',f_b);
-fn_e = factor_node('fn:e',f_e);
-fn_abe = factor_node('fn:abe',f_abe);
-fn_ja = factor_node('fn:ja',f_ja);
-fn_ma = factor_node('fn:ma',f_ma);
-
-vn_b = variable_node('vn:b',2);
-vn_e = variable_node('vn:e',2);
-vn_a = variable_node('vn:a',2);
-vn_j = variable_node('vn:j',2);
-vn_m = variable_node('vn:m',2);
-
-fn_b.addNode(vn_b);
-fn_e.addNode(vn_e);
-fn_abe.addNode(vn_a);
-fn_abe.addNode(vn_b);
-fn_abe.addNode(vn_e);
-fn_ja.addNode(vn_j);
-fn_ja.addNode(vn_a);
-fn_ma.addNode(vn_m);
-fn_ma.addNode(vn_a);
-
-vn_b.addNode(fn_b);
-vn_b.addNode(fn_abe);
-vn_e.addNode(fn_e);
-vn_e.addNode(fn_abe);
-vn_a.addNode(fn_abe);
-vn_a.addNode(fn_ja);
-vn_a.addNode(fn_ma);
-vn_j.addNode(fn_ja);
-vn_m.addNode(fn_ma);
-
-%find marginal probability of alarm
-vn_b.passMessagesIn
-disp(' ');
-vn_b.passMessagesOut
-%%
-
-vn_a.getMarginalDistribution
-
-vn_b.setValue(1);
-vn_m.setValue(1);
-
-for i = 1 : 2
-    vn_b.loopy_bp;
-    vn_b.setNotUpdated;
-end
-
-vn_a.getMarginalDistribution
-
-vn_b.passMessagesIn
-disp(' ');
-vn_b.passMessagesOut
-vn_a.getMarginalDistribution
+%display some marginal distributions
+get_marginal_distribution(vn_b)
